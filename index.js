404d8b5ed5f31b9b6477be9b285119022a6a1c49
Paweł Babiuch <szkieletor73@gmail.com>
2019-03-16 17:24:43 +0100

    Code optimization

--- index.js
+++ index.js
@@ -11,24 +11,17 @@ function createEmbed(type, leader = null) {
   embed = new Discord.RichEmbed()
   switch(type){
     case "cult":
-      embed.setColor(loc.cult.color)
-      embed.addField(loc.cult.name, loc.cult.desc + loc.generic.voice + loc.generic.end)
-      embed.setFooter(loc.cult.footer + leader.username + loc.generic.footer)
+    case "void":
+      embed.setColor(loc[type].color)
+      embed.addField(loc[type].name, loc[type].desc + loc.generic.voice + loc.generic.end)
+      embed.setFooter(loc[type].footer + leader.username + loc.generic.footer)
       break;
     case "cultEnd":
-      embed.setColor(loc.cult.color)
-      embed.addField(loc.cult.name, loc.generic.ended)
-      embed.setFooter(loc.cult.footer + leader.username + loc.generic.footer)
-      break;
-    case "void":
-      embed.setColor(loc.void.color)
-      embed.addField(loc.void.name, loc.void.desc + loc.generic.voice + loc.generic.end)
-      embed.setFooter(loc.void.footer + leader.username + loc.generic.footer)
-      break;
     case "voidEnd":
-      embed.setColor(loc.void.color)
-      embed.addField(loc.void.name, loc.generic.ended)
-      embed.setFooter(loc.void.footer + leader.username + loc.generic.footer)
+      console.log(type.substring(0, type.length - 4))
+      embed.setColor(loc[type.substring(0, type.length - 3)].color)
+      embed.addField(loc[type.substring(0, type.length - 3)].name, loc.generic.ended)
+      embed.setFooter(loc[type.substring(0, type.length - 3)].footer + leader.username + loc.generic.footer)
       break;
   }
   return embed;
@@ -36,13 +29,14 @@ function createEmbed(type, leader = null) {
 
 function sendRaid(type, leader, channel) {
   let filter = (reaction, user) => { return reaction.emoji.name === "❌" && user.id === leader.id; }
+  
+  channel.send("@here - " + type.charAt(0).toUpperCase() + type.slice(1) + " run started by " + leader + ".")
 
   channel.send(createEmbed(type, leader))
     .then(function (call) {
       call.react("✅").then().catch(console.error)
       call.react("🔑").then().catch(console.error)
       call.react("❌").then().catch(console.error)
-      console.log("Sent, reacted");
 
       call.awaitReactions(filter, { max: 1 })
         .then(function (collected) {
@@ -62,16 +56,16 @@ client.on('message', msg => {
     cmd = msg.content.substring(1, msg.content.length)
     console.log(cmd)
 
-    if (cmd == "void") {
-      msg.delete()
-      console.log("Sending void")
-      sendRaid("void", msg.author, msg.channel);
-    }
+    if(cmd.split(" ")[0] == "raid") {
+      if (cmd.split(" ")[1] == "void") {
+        msg.delete()
+        sendRaid("void", msg.author, msg.channel);
+      }
 
-    if (cmd == "cult") {
-      msg.delete()
-      console.log("Sending cult")
-      sendRaid("cult", msg.author, msg.channel);
+      if (cmd.split(" ")[1] == "cult") {
+        msg.delete()
+        sendRaid("cult", msg.author, msg.channel);
+      }
     }
 
     if (cmd == "listEmoji" && msg.author.id == 95310448697548800){
